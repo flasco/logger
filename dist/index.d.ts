43bf2d4c91ec1e4e52237c3bb002599ba282dec2
lear <1325925707@qq.com>
2019-06-29 15:57:07 +0800

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,2 @@
-declare class Logger {
-    private _needTime;
-    time: Logger | undefined;
-    constructor(needTime?: boolean);
-    private log;
-    success(...args: any[]): void;
-    warn(...args: any[]): void;
-    info(...args: any[]): void;
-    error(...args: any[]): void;
-}
-declare const logger: Logger;
+declare const logger: any;
 export = logger;
